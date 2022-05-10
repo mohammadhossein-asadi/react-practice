b7d169dc5158b4037de476f9cff0b7c2c83cce9b
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-10 21:20:11 +0430

    commit

--- src/components/StateProps.js
+++ src/components/StateProps.js
@@ -5,12 +5,15 @@ import "../App.css";
 function StateProps() {
   // Write Javascript Here
   const [counter, setCounter] = useState(0);
+  const [toggle, setToggle] = useState(false);
 
-  const incrementer = () => {};
+  const incrementer = () => {
+    setCounter((pre) => pre + 1);
+  };
 
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1 className={toggle ? "active" : ""}>Hello React</h1>
       <h2>Counter {counter}</h2>
       <button onClick={incrementer}>Click</button>
     </div>
