c8a5fec9a0acae3f9cb60ec281c8ad9f9f3762f5
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-10 19:28:56 +0430

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import "./App.css";
 import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const decrementCount = () => setCount(count - 1);
+  const incrementCount = () => setCount(count + 1);
+
   return (
     <div className="main">
-      <button className="btn btn-primary">-</button>
-      <h3>0</h3>
-      <button className="btn btn-primary">+</button>
+      <button className="btn btn-primary" onClick={decrementCount}>
+        -
+      </button>
+      <h3>{count}</h3>
+      <button className="btn btn-primary" onClick={incrementCount}>
+        +
+      </button>
     </div>
   );
 }
