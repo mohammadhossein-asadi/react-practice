9e4f90a410b3da50ae9c3794c4ec18877c9e6816
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-10 20:00:55 +0430

    commit

--- src/components/StateProps.js
+++ src/components/StateProps.js
@@ -1,11 +1,13 @@
 // Import components
 import Nav from "./Nav";
+import Tweets from "./Tweets";
 
 function StateProps() {
   return (
     <div>
       <h1>Hello</h1>
       <Nav />
+      <Tweets />
     </div>
   );
 }
