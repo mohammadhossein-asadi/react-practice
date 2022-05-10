c3afa32cd3a3b48f226fd40d669096399b5f2562
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-10 19:57:33 +0430

    commit

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+const Nav = () => (
+  <nav>
+    <li>
+      <a>Home</a>
+    </li>
+    <li>
+      <a>Personal</a>
+    </li>
+    <li>
+      <a>Analyrics</a>
+    </li>
+    <li>
+      <a>Tweets</a>
+    </li>
+  </nav>
+);
