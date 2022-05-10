58f9d2d34aec4a9cfa048fd910a396e42107721c
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-10 20:52:46 +0430

    commit

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+const Nav = () => (
+  <nav>
+    <li>
+      <a>Home</a>
+    </li>
+    <li>
+      <a>Personal</a>
+    </li>
+    <li>
+      <a>Analyrics</a>
+    </li>
+    <li>
+      <a>Tweets</a>
+    </li>
+  </nav>
+);
+
+export default Nav;
---
+++ src/components/Tweet.js
@@ -0,0 +1,8 @@
+const Tweet = (props) => (
+  <div>
+    <h1>{props.name}</h1>
+    <h3>{props.tweet}</h3>
+  </div>
+);
+
+export default Tweet;
